d8c823f4719a4f4895d3529cef6b7020be1a4df8
Jan Gromeš <jgromes@users.noreply.github.com>
2017-01-29 17:41:25 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,103 @@
+# LoRaLib
+
+## Arduino library for LoRa modules based on SX1278 chip
+
+DISCLAIMER: This library is provided 'AS IS'. See `license.txt` for details.
+
+This library enables easy long range communaction with SX1278-based LoRa modules. This library was designed to be used with LoRenz Rev.B shields or modules (see [LoRenz repository](https://github.com/jgromes/LoRenz) for details and Eagle files). However, they are not required and this library can be used with any LoRa module, as long as it is based on the SX1278 chip.
+
+## Library reference
+The following is a list of currently implemented features and their description, see `/examples/` for examples of their usage.
+
+* `LoRa(int nss, uint8_t bw, uint8_t cr, uint8_t sf)`
+
+  Default constructor, used to create the LoRa object.  
+  Usage:
+  
+  * Single LoRa module connected:
+  
+    ```c++
+    LoRa lora;
+    ```
+    
+    This will create a `lora` instance of the `LoRa` class with default values of bandwidth, coding rate and spreading factor. See Table 1. for their recommended values.
+  
+  * Multiple LoRa modules connected simultaneously:
+    
+    ```c++
+    LoRa lora0(7);
+    LoRa lora1(6);
+    LoRa lora2(5);
+    LoRa lora3(4);
+    ```
+    
+    Multiple LoRenz Rev.B shields/modules can be connected to a single Arduino, however, on each of them, a different position on the NSS pin header has to be shorted out. Since there are 4 positions on the NSS pin header, the maximum of simultaneously connected LoRenz Rev.B shields/modules is 4. Any number of other LoRa modules can be connected and used with this library as long as their SPI NSS connection is not the same as the ones already used for LoRenz Rev.B shield(s)/module(s)
+
+* `int tx(packet* pack)`
+
+  This function will attempt to transmit the `packet pack` in LoRa mode. For information on the packet structure, see the third part of this document. In case the supplied packet is larger than 256 bytes, the function will return -1. In case of a successful transmission, the function will return 0.
+
+* `packet* rx(uint8_t mode)`
+
+  This function will attempt to receive incoming data in LoRa mode. For information on the packet structure, see the third part of this document. The default mode for this function is `RXSINGLE` mode: the function will be terminated after successful reception of a single packet, this is the recommended RX mode. In `RXCONTINUOUS` mode, the function will keep on going until terminated externally.
+
+* `void setMode(uint8_t mode)`
+
+  This function will set the LoRa module into a specified mode. The available modes are: `SLEEP`, `STANDBY`, `FSTX`, `FSRX`, `TX`, `RXCONTINUOUS`, `RXSINGLE` and `CAD`. For details on these modes, please refer to the SX1278 datasheet.
+
+# Packet structure
+
+Because of the SX1278 internal FIFO buffer limitations, the maximum size of a single packet is 256 bytes. The following packet structure is a part of this library:
+
+```
+struct packet {
+  uint8_t source[8];
+  uint8_t destination[8];
+  const char* data;
+};
+```
+
+The `source` and `destination` arrays are the addresses of the source and the destination LoRa modules. These addresses are in the form of an array of 8 bytes and can be supplied in the form of an address string, e.g. "01:23:45:67:89:AB:CD:EF".  
+The `data` is a null-terminated string of up to 240 characters.
+
+The values of all of the above can be set and retrieved by the following functions:
+
+* `void setPacketSource(packet* pack, uint8_t* address)`
+
+  Set the packet source in the form of an array of 8 `byte` numbers.
+
+* `void setPacketSourceStr(packet* pack, const char* address)`
+
+  Set the packet source in the form of an address string.
+
+* `uint8_t* getPacketSource(packet* pack)`
+
+  Retrieve the packet source in the form of an array of 8 `byte` numbers.
+
+* `const char* getPacketSourceStr(packet* pack)`
+
+  Retrieve the packet source in the form of an address string.
+
+* `void setPacketDestination(packet* pack, uint8_t* address)`
+
+  Set the packet destination in the form of an array of 8 `byte` numbers.
+
+* `void setPacketDestinationStr(packet* pack, const char* address)`
+
+  Set the packet destination in the form of an address string.
+
+* `uint8_t* getPacketDestination(packet* pack)`
+
+  Retrieve the packet destination in the form of an array of 8 `byte` numbers.
+
+* `const char* getPacketDestinationStr(packet* pack)`
+
+  Retrieve the packet destination in the form of an address string.
+
+* `void setPacketData(packet* pack, const char* data)`
+
+  Set the packet data in the form of a null-terminated string of up to 240 characters.
+
+* `const char* getPacketData(packet* pack)`
+
+  Retrieve the packet data in the form of a null-terminated string of up to 240 characters.
---
+++ src/LoRaLib.cpp
@@ -0,0 +1,318 @@
+#include "LoRaLib.h"
+
+LoRa::LoRa(int nss, uint8_t bw, uint8_t cr, uint8_t sf) {
+  _nss = nss;
+  
+  pinMode(_nss, OUTPUT);
+  pinMode(_sck, OUTPUT);
+  pinMode(_miso, INPUT);
+  pinMode(_mosi, OUTPUT);
+  pinMode(_reset, OUTPUT);
+  pinMode(_dio0, INPUT);
+  
+  SPI.begin();
+  randomSeed(analogRead(5));
+  
+  if(EEPROM.read(8) == 0) {
+    generateLoRaAdress();
+  }
+  
+  for(uint8_t i = 0; i < 8; i++) {
+    _LoRaAddress[i] = EEPROM.read(i);
+  }
+  
+  config(bw, cr, sf);
+}
+
+int LoRa::tx(packet* pack) {
+  setMode(SX1278_STANDBY);
+  setRegValue(SX1278_REG_PA_DAC, SX1278_PA_BOOST_ON, 2, 0);
+  setRegValue(SX1278_REG_HOP_PERIOD, SX1278_HOP_PERIOD_OFF);
+  setRegValue(SX1278_REG_DIO_MAPPING_1, SX1278_DIO0_TX_DONE, 7, 6);
+  clearIRQFlags();
+  setRegValue(SX1278_REG_IRQ_FLAGS_MASK, SX1278_MASK_IRQ_FLAG_TX_DONE);
+  
+  uint8_t packetLength = getPacketLength(pack);
+  if(packetLength > 256) {
+    return -1;
+  }
+  setRegValue(SX1278_REG_PAYLOAD_LENGTH, packetLength);
+  setRegValue(SX1278_REG_FIFO_TX_BASE_ADDR, SX1278_FIFO_TX_BASE_ADDR_MAX);
+  setRegValue(SX1278_REG_FIFO_ADDR_PTR, SX1278_FIFO_TX_BASE_ADDR_MAX);
+  
+  writeRegisterBurst(SX1278_REG_FIFO, pack->source, 8);
+  writeRegisterBurst(SX1278_REG_FIFO, pack->destination, 8);
+  writeRegisterBurstStr(SX1278_REG_FIFO, pack->data, packetLength - 16);
+  
+  setMode(SX1278_TX);
+  while(!digitalRead(_dio0));
+  clearIRQFlags();
+  
+  return 0;
+}
+
+packet* LoRa::rx(uint8_t mode, uint8_t packetLength) {
+  setMode(SX1278_STANDBY);
+  if(mode == SX1278_RXSINGLE) {
+    setRegValue(SX1278_REG_PA_DAC, SX1278_PA_BOOST_OFF);
+    setRegValue(SX1278_REG_HOP_PERIOD, SX1278_HOP_PERIOD_MAX);
+    setRegValue(SX1278_REG_DIO_MAPPING_1, SX1278_DIO0_RX_DONE | SX1278_DIO1_RX_TIMEOUT, 7, 5);
+    clearIRQFlags();
+    setRegValue(SX1278_REG_IRQ_FLAGS_MASK, (SX1278_MASK_IRQ_FLAG_PAYLOAD_CRC_ERROR & SX1278_MASK_IRQ_FLAG_VALID_HEADER));
+    
+    setRegValue(SX1278_REG_FIFO_RX_BASE_ADDR, SX1278_FIFO_RX_BASE_ADDR_MAX);
+    setRegValue(SX1278_REG_FIFO_ADDR_PTR, SX1278_FIFO_RX_BASE_ADDR_MAX);
+    
+    setMode(SX1278_RXSINGLE);
+    while(!digitalRead(_dio0)) {
+      if(digitalRead(_dio1)) {
+        return nullptr;
+      }
+    }
+    
+    if(readRegister(SX1278_REG_IRQ_FLAGS)) {
+      return nullptr;
+    }
+    
+    uint8_t sf = getRegValue(SX1278_REG_MODEM_CONFIG_2, 7, 4) & 0b00001111;
+    if(sf != SX1278_SF_6) {
+      packetLength = getRegValue(SX1278_REG_RX_NB_BYTES);
+    }
+    
+    packet* pack = new packet;
+    const char* data = new char[packetLength - 16];
+    
+    for(uint8_t i = 0; i < 8; i++) {
+      pack->source[i] = readRegister(SX1278_REG_FIFO);
+    }
+    for(uint8_t i = 0; i < 8; i++) {
+      pack->destination[i] = readRegister(SX1278_REG_FIFO);
+    }
+    data = readRegisterBurstStr(SX1278_REG_FIFO, packetLength - 16);
+    pack->data = data;
+    
+    return(pack);
+  } else if (mode == SX1278_RXCONTINUOUS) {
+    
+  }
+  
+  return nullptr;
+}
+
+void LoRa::setMode(uint8_t mode) {
+  setRegValue(SX1278_REG_OP_MODE, mode, 2, 0);
+}
+
+void LoRa::setPacketSource(packet* pack, uint8_t* address) {
+  for(uint8_t i = 0; i < 8; i++) {
+    pack->source[i] = address[i];
+  }
+}
+
+void LoRa::setPacketSourceStr(packet* pack, const char* address) {
+  for(uint8_t i = 0; i < 8; i++) {
+    pack->source[i] = (parseByte(address[3*i]) << 4) | parseByte(address[3*i + 1]);
+  }
+}
+
+uint8_t* LoRa::getPacketSource(packet* pack) {
+  uint8_t src[24];
+  for(uint8_t i = 0; i < 8; i++) {
+    src[i] = pack->source[i];
+  }
+  return(src);
+}
+
+const char* LoRa::getPacketSourceStr(packet* pack) {
+  char* src = new char[24];
+  for(uint8_t i = 0; i < 8; i++) {
+    src[3*i] = reparseChar(pack->source[i] >> 4);
+    src[3*i+1] = reparseChar(pack->source[i] & 0x0F);
+    src[3*i+2] = ':';
+  }
+  src[23] = '\0';
+  return(src);
+}
+
+void LoRa::setPacketDestination(packet* pack, uint8_t* address) {
+  for(uint8_t i = 0; i < 8; i++) {
+    pack->destination[i] = address[i];
+  }
+}
+
+void LoRa::setPacketDestinationStr(packet* pack, const char* address) {
+  for(uint8_t i = 0; i < 8; i++) {
+    pack->destination[i] = (parseByte(address[3*i]) << 4) | parseByte(address[3*i + 1]);
+  }
+}
+
+uint8_t* LoRa::getPacketDestination(packet* pack) {
+  uint8_t dest[24];
+  for(uint8_t i = 0; i < 8; i++) {
+    dest[i] = pack->destination[i];
+  }
+  return(dest);
+}
+
+const char* LoRa::getPacketDestinationStr(packet* pack) {
+  char* des = new char[24];
+  for(uint8_t i = 0; i < 8; i++) {
+    dest[3*i] = reparseChar(pack->destination[i] >> 4);
+    dest[3*i+1] = reparseChar(pack->destination[i] & 0x0F);
+    dest[3*i+2] = ':';
+  }
+  dest[23] = '\0';
+  return(dest);
+}
+
+void LoRa::setPacketData(packet* pack, const char* data) {
+  pack->data = data;
+}
+
+const char* LoRa::getPacketData(packet* pack) {
+  return(pack->data);
+}
+
+void LoRa::config(uint8_t bw, uint8_t cr, uint8_t sf) {
+  setMode(SX1278_SLEEP);
+  
+  setRegValue(SX1278_REG_OP_MODE, SX1278_LORA, 7, 7);
+  
+  setRegValue(SX1278_REG_FRF_MSB, SX1278_FRF_MSB);
+  setRegValue(SX1278_REG_FRF_MID, SX1278_FRF_MID);
+  setRegValue(SX1278_REG_FRF_LSB, SX1278_FRF_LSB);
+  
+  setRegValue(SX1278_REG_PA_CONFIG, SX1278_PA_SELECT_BOOST | SX1278_MAX_POWER | SX1278_OUTPUT_POWER);
+  setRegValue(SX1278_REG_OCP, SX1278_OCP_ON | SX1278_OCP_TRIM, 5, 0);
+  setRegValue(SX1278_REG_LNA, SX1278_LNA_GAIN_1 | SX1278_LNA_BOOST_HF_ON);
+  
+  if(sf == SX1278_SF_6) {
+    setRegValue(SX1278_REG_MODEM_CONFIG_2, SX1278_SF_6 | SX1278_TX_MODE_SINGLE | SX1278_RX_CRC_MODE_ON | SX1278_RX_TIMEOUT_MSB);
+    setRegValue(SX1278_REG_MODEM_CONFIG_1, bw | cr | SX1278_HEADER_IMPL_MODE);
+    setRegValue(SX1278_REG_DETECT_OPTIMIZE, SX1278_DETECT_OPTIMIZE_SF_6, 2, 0);
+    setRegValue(SX1278_REG_DETECTION_THRESHOLD, SX1278_DETECTION_THRESHOLD_SF_6);
+  } else {
+    setRegValue(SX1278_REG_MODEM_CONFIG_2, sf | SX1278_TX_MODE_SINGLE | SX1278_RX_CRC_MODE_ON | SX1278_RX_TIMEOUT_MSB);
+    setRegValue(SX1278_REG_MODEM_CONFIG_1, bw | cr | SX1278_HEADER_EXPL_MODE);
+    setRegValue(SX1278_REG_DETECT_OPTIMIZE, SX1278_DETECT_OPTIMIZE_SF_7_12, 2, 0);
+    setRegValue(SX1278_REG_DETECTION_THRESHOLD, SX1278_DETECTION_THRESHOLD_SF_7_12);
+  }
+  
+  setRegValue(SX1278_REG_SYMB_TIMEOUT_LSB, SX1278_RX_TIMEOUT_LSB);
+  setRegValue(SX1278_REG_PREAMBLE_MSB, SX1278_PREAMBLE_LENGTH_MSB);
+  setRegValue(SX1278_REG_PREAMBLE_LSB, SX1278_PREAMBLE_LENGTH_LSB);
+  
+  setMode(SX1278_STANDBY);
+}
+
+uint8_t LoRa::setRegValue(uint8_t reg, uint8_t value, uint8_t msb, uint8_t lsb) {
+  if((msb > 7) || (lsb > 7)) {
+    return 0xFF;
+  }
+  uint8_t currentValue = readRegister(reg);
+  uint8_t newValue = currentValue & ((0b11111111 << (msb + 1)) | (0b11111111 >> (8 - lsb)));
+  writeRegister(reg, newValue | value);
+}
+
+uint8_t LoRa::getRegValue(uint8_t reg, uint8_t msb, uint8_t lsb) {
+  if((msb > 7) || (lsb > 7)) {
+    return 0xFF;
+  }
+  uint8_t rawValue = readRegister(reg);
+  uint8_t maskedValue = rawValue & ((0b11111111 << lsb) | (0b11111111 >> (7 - msb)));
+  return(maskedValue);
+}
+
+void LoRa::clearIRQFlags(void) {
+  writeRegister(SX1278_REG_IRQ_FLAGS, 0b11111111);
+}
+
+uint8_t* LoRa::readRegisterBurst(uint8_t reg, uint8_t numBytes) {
+  uint8_t* inBytes = new uint8_t[numBytes];
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_READ);
+  for(uint8_t i = 0; i< numBytes; i++) {
+    inBytes[i] = SPI.transfer(reg);
+  }
+  digitalWrite(_nss, HIGH);
+  return(inBytes);
+}
+
+const char* LoRa::readRegisterBurstStr(uint8_t reg, uint8_t numBytes) {
+  char* inBytes = new char[numBytes];
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_READ);
+  for(uint8_t i = 0; i< numBytes; i++) {
+    inBytes[i] = SPI.transfer(reg);
+  }
+  digitalWrite(_nss, HIGH);
+  return(inBytes);
+}
+
+void LoRa::writeRegisterBurst(uint8_t reg, uint8_t* data, uint8_t numBytes) {
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_WRITE);
+  for(uint8_t i = 0; i< numBytes; i++) {
+    SPI.transfer(data[i]);
+  }
+  digitalWrite(_nss, HIGH);
+}
+
+void LoRa::writeRegisterBurstStr(uint8_t reg, const char* data, uint8_t numBytes) {
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_WRITE);
+  for(uint8_t i = 0; i< numBytes; i++) {
+    SPI.transfer(data[i]);
+  }
+  digitalWrite(_nss, HIGH);
+}
+
+uint8_t LoRa::readRegister(uint8_t reg) {
+  uint8_t inByte;
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_READ);
+  inByte = SPI.transfer(0x00);
+  digitalWrite(_nss, HIGH);
+  return(inByte);
+}
+
+void LoRa::writeRegister(uint8_t reg, uint8_t data) {
+  digitalWrite(_nss, LOW);
+  SPI.transfer(reg | SX1278_WRITE);
+  SPI.transfer(data);
+  digitalWrite(_nss, HIGH);
+}
+
+void LoRa::generateLoRaAdress(void) {
+  for(uint8_t i = 0; i < 8; i++) {
+    EEPROM.write(i, (uint8_t)random(0, 256));
+  }
+}
+
+uint8_t LoRa::getPacketLength(packet* pack) {
+  for(uint8_t i = 0; i < 240; i++) {
+    if(pack->data[i] == '\0') {
+      return(i + 17);
+    }
+  }
+}
+
+uint8_t LoRa::parseByte(char c) {
+  if((c >= 48) && (c <= 57)) {
+    return(c - 48);
+  } else if((c >= 65) && (c <= 70)) {
+    return(c - 55);
+  } else if((c >= 97) && (c <= 102)) {
+    return(c - 87);
+  }
+  return 0;
+}
+
+char LoRa::reparseChar(uint8_t b) {
+  if((b >= 0) && (b <= 9)) {
+    return(b + 48);
+  } else if((b >= 10) && (b <= 16)) {
+    return(b + 55);
+  }
+  return 0;
+}
