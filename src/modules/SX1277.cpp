7e61b0ec4ebd891f79ad64cabf2cb1771dace43f
Jan Gromeš <jgromes@users.noreply.github.com>
2018-07-22 08:45:11 +0200

    Fix for section 2.3 of SX1278 errata

--- src/modules/SX1277.cpp
+++ src/modules/SX1277.cpp
@@ -22,6 +22,54 @@ uint8_t SX1277::setFrequency(float freq) {
     }
   }
   
+  // mitigation of receiver spurious response
+  // see SX1276/77/78 Errata, section 2.3 for details
+  if(_bw == 7.8) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x48);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 7.8;
+  } else if(_bw == 10.4) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x44);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 10.4;
+  } else if(_bw == 15.6) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x44);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 15.6;
+  } else if(_bw == 20.8) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x44);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 20.8;
+  } else if(_bw == 31.25) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x44);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 31.25;
+  } else if(_bw == 41.7) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x44);
+    _mod->SPIsetRegValue(0x30, 0x00);
+    freq += 41.7;
+  } else if(_bw == 62.5) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x40);
+    _mod->SPIsetRegValue(0x30, 0x00);
+  } else if(_bw == 125.0) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x40);
+    _mod->SPIsetRegValue(0x30, 0x00);
+  } else if(_bw == 250.0) {
+    _mod->SPIsetRegValue(0x31, 0b0000000, 7, 7);
+    _mod->SPIsetRegValue(0x2F, 0x40);
+    _mod->SPIsetRegValue(0x30, 0x00);
+  } else if(_bw == 500.0) {
+    _mod->SPIsetRegValue(0x31, 0b1000000, 7, 7);
+  }
+  
   // set frequency and if successful, save the new setting
   return(SX127x::setFrequencyRaw(freq));
 }
