2f04d58665844a85ca55d4305a28c5363392a6b7
Jan Gromeš <jgromes@users.noreply.github.com>
2018-07-22 08:19:47 +0200

    Fix for section 2.1 of SX1278 errata

--- src/modules/SX1278.cpp
+++ src/modules/SX1278.cpp
@@ -52,6 +52,18 @@ uint8_t SX1278::setFrequency(float freq) {
     return(ERR_INVALID_FREQUENCY);
   }
   
+  // sensitivity optimization for 500kHz bandwidth
+  // see SX1276/77/78 Errata, section 2.1 for details
+  if(_bw == 500.0) {
+    if((freq >= 862.0) && (freq <= 1020.0)) {
+      _mod->SPIwriteRegister(0x36, 0x02);
+      _mod->SPIwriteRegister(0x3a, 0x64);
+    } else if((freq >= 410.0) && (freq <= 525.0)) {
+      _mod->SPIwriteRegister(0x36, 0x03);
+      _mod->SPIwriteRegister(0x3a, 0x65);
+    }
+  }
+  
   // set frequency
   return(SX127x::setFrequencyRaw(freq));
 }
