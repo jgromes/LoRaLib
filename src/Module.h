a64d76be3a7f61d684d0369ce43c142bd088aa2b
Jan Gromeš <jgromes@users.noreply.github.com>
2018-07-23 11:01:58 +0200

    Added SPI verification after write

--- src/Module.h
+++ src/Module.h
@@ -12,14 +12,15 @@ class Module {
   public:
     Module(int cs = 7, int int0 = 2, int int1 = 3);
     
-    uint8_t init(uint8_t interface, uint8_t gpio);
+    void init(uint8_t interface, uint8_t gpio);
     
-    uint8_t SPIgetRegValue(uint8_t reg, uint8_t msb = 7, uint8_t lsb = 0);
-    uint8_t SPIreadRegisterBurst(uint8_t reg, uint8_t numBytes, uint8_t* inBytes);
-    uint8_t SPIreadRegisterBurstStr(uint8_t reg, uint8_t numBytes, char* str);
+    int16_t SPIgetRegValue(uint8_t reg, uint8_t msb = 7, uint8_t lsb = 0);
+    int16_t SPIsetRegValue(uint8_t reg, uint8_t value, uint8_t msb = 7, uint8_t lsb = 0);
+    
+    void SPIreadRegisterBurst(uint8_t reg, uint8_t numBytes, uint8_t* inBytes);
+    void SPIreadRegisterBurstStr(uint8_t reg, uint8_t numBytes, char* str);
     uint8_t SPIreadRegister(uint8_t reg);
     
-    uint8_t SPIsetRegValue(uint8_t reg, uint8_t value, uint8_t msb = 7, uint8_t lsb = 0);
     void SPIwriteRegisterBurst(uint8_t reg, uint8_t* data, uint8_t numBytes);
     void SPIwriteRegisterBurstStr(uint8_t reg, const char* data, uint8_t numBytes);
     void SPIwriteRegister(uint8_t reg, uint8_t data);
