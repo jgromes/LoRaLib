186a5a720be2ab8876f605ceb3ec613d6027e8b9
jgromes <jgromes@users.noreply.github.com>
2019-01-01 12:46:54 +0100

    Extended PhysicalLayer

--- src/protocols/PhysicalLayer.h
+++ src/protocols/PhysicalLayer.h
@@ -6,19 +6,21 @@
 class PhysicalLayer {
   public:
     // constructor
-    PhysicalLayer(float crysFreq, uint8_t divExp) { _crystalFreq = crysFreq; _divExponent = divExp; }
+    PhysicalLayer(float crysFreq, uint8_t divExp);
     
     // basic methods
-    virtual int16_t transmitDirect(uint32_t FRF = 0) = 0;
-    virtual int16_t receiveDirect() = 0;
-    virtual int16_t transmit(const char* data, uint8_t addr = 0) = 0;
+    int16_t transmit(String& str, uint8_t addr = 0);
+    int16_t transmit(const char* str, uint8_t addr = 0);
     virtual int16_t transmit(uint8_t* data, size_t len, uint8_t addr = 0) = 0;
+    int16_t receive(String& str, size_t len = 0);
     virtual int16_t receive(uint8_t* data, size_t len) = 0;
     
+    virtual int16_t transmitDirect(uint32_t FRF = 0) = 0;
+    virtual int16_t receiveDirect() = 0;
     // configuration methods
     virtual int16_t setFrequencyDeviation(float freqDev) = 0;
-    float getCrystalFreq() { return(_crystalFreq); }
-    uint8_t getDivExponent() { return(_divExponent); }
+    float getCrystalFreq();
+    uint8_t getDivExponent();
 
   private:
     float _crystalFreq;
