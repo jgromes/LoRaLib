f93186e78bb2f0bbdaedbba989583c6913294aa7
jgromes <jan.gromes@gmail.com>
2019-05-24 20:23:25 +0200

    Updated Doxygen comments

--- src/Module.h
+++ src/Module.h
@@ -18,152 +18,152 @@
 
 /*!
   \class Module
-  
+
   \brief Implements all common low-level SPI methods to control the %LoRa chip. The base class SX127x contains private instance of this class.
 */
 class Module {
   public:
-  
+
     /*!
       \brief Default constructor. Called internally when creating new LoRa instance.
-      
+
       \param cs Arduino pin that will be used as chip select signal for SPI. Defaults to 4 on ESP32/ESP8266 boards, or to 10 on other architectures.
-      
-      \param int0 Arduino pin that will be used as interrupt/GPIO 0. Connect to SX127x/RFM9x pin DIO0. 
+
+      \param int0 Arduino pin that will be used as interrupt/GPIO 0. Connect to SX127x/RFM9x pin DIO0.
       Does not need to be connected to Arduino interrupt pin, unless Arduino sketch is using interrupt-driven transmit/receive methods.
-      
-      \param int1 Arduino pin that will be used as interrupt/GPIO 1. Connect to SX127x/RFM9x pin DIO1. 
+
+      \param int1 Arduino pin that will be used as interrupt/GPIO 1. Connect to SX127x/RFM9x pin DIO1.
       Does not need to be connected to Arduino interrupt pin, unless Arduino sketch is using interrupt-driven transmit/receive methods.
-      
+
       \param spi SPIClass instance that will be used for SPI bus control. This can be hardware SPI or some software SPI driver.
     */
     Module(int cs = LORALIB_DEFAULT_SPI_CS, int int0 = 2, int int1 = 3, SPIClass& spi = SPI);
-    
+
     /*!
       \brief Initialization method. Called internally when connecting to the %LoRa chip and should not be called explicitly from Arduino code.
-      
+
       \param interface %Module interface that should be used. Required for RadioLib compatibility, will always be set to USE_SPI.
-      
+
       \param gpio Determines which interrupt/GPIO should be used. Required for RadioLib compatibility, will always be set to INT_BOTH.
     */
     void init(uint8_t interface, uint8_t gpio);
-    
+
     /*!
       \brief Termination method. Called internally when required %LoRa chip is not found and should not be called explicitly from Arduino code.
     */
     void term();
-    
-    
-    
+
+
+
     /*!
       \brief SPI read method that automatically masks unused bits. This method is the preferred SPI read mechanism.
-      
+
       \param reg Address of SPI register to read.
-      
+
       \param msb Most significant bit of the register variable. Bits above this one will be masked out.
-      
+
       \param lsb Least significant bit of the register variable. Bits below this one will be masked out.
-      
+
       \returns Masked register value or status code.
     */
     int16_t SPIgetRegValue(uint8_t reg, uint8_t msb = 7, uint8_t lsb = 0);
-    
+
     /*!
       \brief Overwrite-safe SPI write method with verification. This method is the preferred SPI write mechanism.
-      
+
       \param reg Address of SPI register to write.
-      
+
       \param value Single byte value that will be written to the SPI register.
-      
+
       \param msb Most significant bit of the register variable. Bits above this one will not be affected by the write operation.
-      
+
       \param lsb Least significant bit of the register variable. Bits below this one will not be affected by the write operation.
-      
+
       \param checkInterval Number of milliseconds between register writing and verification reading. Some registers need up to 10ms to process the change.
-      
+
       \returns \ref status_codes
     */
     int16_t SPIsetRegValue(uint8_t reg, uint8_t value, uint8_t msb = 7, uint8_t lsb = 0, uint8_t checkInterval = 2);
-    
-    
-    
+
+
+
     /*!
       \brief SPI burst read method.
-      
+
       \param reg Address of SPI register to read.
-      
+
       \param numBytes Number of bytes that will be read.
-      
-      \param inBytes Ponter to array that will hold the read data.
+
+      \param inBytes Pointer to array that will hold the read data.
     */
     void SPIreadRegisterBurst(uint8_t reg, uint8_t numBytes, uint8_t* inBytes);
-    
+
     /*!
       \brief SPI basic read method. Use of this method is reserved for special cases, SPIgetRegValue should be used instead.
-      
+
       \param reg Address of SPI register to read.
-      
+
       \returns Value that was read from register.
     */
     uint8_t SPIreadRegister(uint8_t reg);
-    
-    
-    
+
+
+
     /*!
       \brief SPI burst write method.
-      
+
       \param reg Address of SPI register to write.
-      
-      \param data Ponter to array that holds the data that will be written.
-      
+
+      \param data Pointer to array that holds the data that will be written.
+
       \param numBytes Number of bytes that will be written.
     */
     void SPIwriteRegisterBurst(uint8_t reg, uint8_t* data, uint8_t numBytes);
-    
+
     /*!
       \brief SPI basic write method. Use of this method is reserved for special cases, SPIsetRegValue should be used instead.
-      
+
       \param reg Address of SPI register to write.
-      
+
       \param data Value that will be written to the register.
     */
     void SPIwriteRegister(uint8_t reg, uint8_t data);
-    
+
     /*!
-      \brief SPI single transfer method. Handles all hardware-related SPI tasks.
-      
+      \brief SPI single transfer method.
+
       \param cmd SPI access command (read/write/burst/...).
-      
+
       \param reg Address of SPI register to transfer to/from.
-      
+
       \param dataOut Data that will be transfered from master to slave.
-      
+
       \param dataIn Data that was transfered from slave to master.
-      
+
       \param numBytes Number of bytes to transfer.
     */
     void SPItransfer(uint8_t cmd, uint8_t reg, uint8_t* dataOut, uint8_t* dataIn, uint8_t numBytes);
-    
-    
+
+
     /*!
-      \brief Access method to get the number of interrupt/GPIO 0 pin.
-      
-      \returns Number of interrupt/GPIO 0 pin configured in the init method.
+      \brief Access method to get the pin number of interrupt/GPIO 0.
+
+      \returns Pin number of interrupt/GPIO 0 configured in the constructor.
     */
     int getInt0() const { return(_int0); }
-    
+
     /*!
-      \brief Access method to get the number of interrupt/GPIO 1 pin.
-      
-      \returns Number of interrupt/GPIO 1 pin configured in the init method.
+      \brief Access method to get the pin number of interrupt/GPIO 1.
+
+      \returns Pin number of interrupt/GPIO 1 configured in the constructor.
     */
     int getInt1() const { return(_int1); }
-  
+
   private:
     int _cs;
     int _int0;
     int _int1;
-    
+
     SPIClass* _spi;
 };
 
--- src/TypeDef.h
+++ src/TypeDef.h
@@ -30,148 +30,148 @@
 
 /*!
   \defgroup status_codes Status Codes
-  
+
   \{
 */
 
-/*! 
+/*!
   \brief No error, method executed successfully.
 */
 #define ERR_NONE                               0
 
-/*! 
-  \brief There was an unexpected, unknown error. If you see this, something went incredibly wrong. 
-  Your Arduino may be possesed, contact your local excorcist to resolve this error.
+/*!
+  \brief There was an unexpected, unknown error. If you see this, something went incredibly wrong.
+  Your Arduino may be possessed, contact your local exorcist to resolve this error.
 */
 #define ERR_UNKNOWN                           -1
 
-/*! 
-  \brief %SX127x chip was not found during initialization. This can be caused by specifying wrong chip type in the constructor 
+/*!
+  \brief %SX127x chip was not found during initialization. This can be caused by specifying wrong chip type in the constructor
   (i.e. calling SX1272 constructor for SX1278 chip) or by a fault in your wiring (incorrect slave select pin).
 */
 #define ERR_CHIP_NOT_FOUND                    -2
 
-/*! 
+/*!
   \brief Deprecated.
 */
 #define ERR_EEPROM_NOT_INITIALIZED            -3
 
-/*! 
-  \brief Packet supplied to transmission method was longer than 255 bytes. 
+/*!
+  \brief Packet supplied to transmission method was longer than 255 bytes.
   %SX127x chips can not send more than 255 bytes in a single %LoRa transmission.
   Length limit is reduced to 63 bytes for FSK transmissions.
 */
 #define ERR_PACKET_TOO_LONG                   -4
 
-/*! 
+/*!
   \brief Timed out waiting for TxDone signal from %SX127x. This error can be caused by faulty wiring (unreliable connection at DIO0).
 */
 #define ERR_TX_TIMEOUT                        -5
 
-/*! 
+/*!
   \brief No packet arrived within a timeframe.
 */
 #define ERR_RX_TIMEOUT                        -6
 
-/*! 
-  \brief The calculated and expected CRCs of received packet do not match. 
+/*!
+  \brief The calculated and expected CRCs of received packet do not match.
   This means that the packet was damaged during transmission and should be sent again.
 */
 #define ERR_CRC_MISMATCH                      -7
 
-/*! 
-  \brief The supplied badwidth value is invalid for this module.
+/*!
+  \brief The supplied bandwidth value is invalid for this module.
 */
 #define ERR_INVALID_BANDWIDTH                 -8
 
-/*! 
+/*!
   \brief The supplied spreading factor value is invalid for this module.
 */
 #define ERR_INVALID_SPREADING_FACTOR          -9
 
-/*! 
+/*!
   \brief The supplied coding rate value is invalid.
 */
 #define ERR_INVALID_CODING_RATE               -10
 
-/*! 
+/*!
   \brief Internal only.
 */
 #define ERR_INVALID_BIT_RANGE                 -11
 
-/*! 
+/*!
   \brief The supplied frequency value is invalid for this module.
 */
 #define ERR_INVALID_FREQUENCY                 -12
 
-/*! 
+/*!
   \brief The supplied output power is invalid.
 */
 #define ERR_INVALID_OUTPUT_POWER              -13
 
-/*! 
-  \brief %LoRa preamble was detected during channel activity detection. 
+/*!
+  \brief %LoRa preamble was detected during channel activity detection.
   This means that there is some %LoRa device currently transmitting in your channel.
 */
 #define PREAMBLE_DETECTED                     -14
 
-/*! 
+/*!
   \brief No %LoRa preambles were detected during channel activity detection. Your channel is free.
 */
 #define CHANNEL_FREE                          -15
 
-/*! 
+/*!
   \brief Real value in SPI register does not match the expected one. This can be caused by faulty SPI wiring.
 */
 #define ERR_SPI_WRITE_FAILED                  -16
 
-/*! 
+/*!
   \brief The supplied current limit value is invalid.
 */
 #define ERR_INVALID_CURRENT_LIMIT             -17
 
-/*! 
+/*!
   \brief The supplied preamble length is invalid.
 */
 #define ERR_INVALID_PREAMBLE_LENGTH           -18
 
-/*! 
+/*!
   \brief The supplied gain value is invalid.
 */
 #define ERR_INVALID_GAIN                      -19
 
-/*! 
-  \brief User tried to execute modem-exclusive method on a wrong modem. 
+/*!
+  \brief User tried to execute modem-exclusive method on a wrong modem.
   For example, this can happen when you try to change %LoRa configuration when FSK modem is active.
 */
 #define ERR_WRONG_MODEM                       -20
 
-/*! 
+/*!
   \brief The supplied bit rate value is invalid.
 */
 #define ERR_INVALID_BIT_RATE                  -21
 
-/*! 
+/*!
   \brief The supplied receiver bandwidth value is invalid.
 */
 #define ERR_INVALID_RX_BANDWIDTH              -22
 
-/*! 
+/*!
   \brief The supplied frequency deviation value is invalid.
 */
 #define ERR_INVALID_FREQUENCY_DEVIATION       -23
 
-/*! 
+/*!
   \brief The supplied FSK sync word is invalid. The sync word is either longer than 8 bytes or contains null-bytes.
 */
 #define ERR_INVALID_SYNC_WORD                 -24
 
-/*! 
+/*!
   \brief The supplied FSK data shaping option is invalid.
 */
 #define ERR_INVALID_DATA_SHAPING              -25
 
-/*! 
+/*!
   \brief The current modulation is invalid for the requested operation.
 */
 #define ERR_INVALID_MODULATION                -26
